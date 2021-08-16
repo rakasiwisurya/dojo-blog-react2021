ab9164aed9daee8bdde271b4dd9bb57be4f5158d
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-08-16 11:46:11 +0700

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello, Raka", e);
+  };
+
+  const handleClickMeAgain = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickMeAgain("mario", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
