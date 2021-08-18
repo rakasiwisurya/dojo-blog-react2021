673216bd8754313c77fe6c4ac95daf5974def4a9
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-08-18 15:22:15 +0700

    useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -23,6 +23,8 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -30,12 +32,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log("use effect ran");
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
